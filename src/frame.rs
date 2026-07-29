48f8530a452e38bfd23cec05f1927113d2c86b2c
Stephen Vizcaino <svizcaino26@gmail.com>
2026-07-29 16:11:24 -0500

    feat: implement Display trait for IcaoAddress

--- src/frame.rs
+++ src/frame.rs
@@ -1,4 +1,4 @@
-use std::ops::RangeInclusive;
+use std::{fmt::Display, ops::RangeInclusive};
 
 use crate::error::AdsbError;
 
@@ -32,6 +32,12 @@ impl IcaoAddress {
 
     pub const fn value(self) -> u32 {
         self.0
+    }
+}
+
+impl Display for IcaoAddress {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:06X}", self.value())
     }
 }
 
