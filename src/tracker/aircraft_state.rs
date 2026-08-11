a8f92b16ee9759622291c188a190f0c3408c00e5
Stephen Vizcaino <svizcaino26@gmail.com>
2026-08-10 20:23:39 -0500

    feat: add aircraft state updating from incomming ADS-B messages

--- src/tracker/aircraft_state.rs
+++ src/tracker/aircraft_state.rs
@@ -1,7 +1,7 @@
 use std::time::{Duration, Instant};
 
 use crate::message::{
-    airborne_velocity::AirborneVelocity, aircraft_identification::AircraftIdentification,
+    airborne_velocity::AirborneVelocity, aircraft_identification::AircraftIdentification, Message,
 };
 
 #[derive(Debug)]
@@ -11,8 +11,31 @@ pub struct AircraftState {
     last_seen: Instant,
 }
 
+impl Default for AircraftState {
+    fn default() -> Self {
+        Self {
+            identification: Option::default(),
+            velocity: Option::default(),
+            last_seen: Instant::now(),
+        }
+    }
+}
+
 impl AircraftState {
     pub fn time_since_last_seen(&self) -> Duration {
         self.last_seen.elapsed()
+    }
+
+    pub fn update(&mut self, msg: Message) {
+        match msg {
+            Message::AircraftIdentification(aircraft_identification) => {
+                self.last_seen = Instant::now();
+                self.identification = Some(aircraft_identification);
+            }
+            Message::AirborneVelocity(airborne_velocity) => {
+                self.last_seen = Instant::now();
+                self.velocity = Some(airborne_velocity);
+            }
+        }
     }
 }
