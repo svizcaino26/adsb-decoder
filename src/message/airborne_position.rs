8f5baca52a6470a78f78414857696503432ef25e
Stephen Vizcaino <svizcaino26@gmail.com>
2026-08-15 16:43:34 -0500

    feat: add common field and spec define constants

--- src/message/airborne_position.rs
+++ src/message/airborne_position.rs
@@ -1,4 +1,15 @@
-use crate::frame::IcaoAddress;
+use std::ops::RangeInclusive;
+
+use crate::{
+    error::AdsbError,
+    frame::{IcaoAddress, RawFrame},
+};
+
+const BAROMETRIC_ALTITUDE_STEP: i32 = 25;
+const BAROMETRIC_ALTITUDE_STEPS_ALT: i32 = 100;
+const ALTITUDE_OFFSET_FT: i32 = 1000;
+const FIELD_ENCODED_ALTITUDE: RangeInclusive<u8> = 41..=52;
+const ALTITUDE_QBIT: RangeInclusive<u8> = 48..=48;
 
 pub struct Feet(i32);
 
