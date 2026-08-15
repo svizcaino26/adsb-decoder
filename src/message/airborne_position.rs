00f5920d2b4e942324fc3e6073930917bfce7d9a
Stephen Vizcaino <svizcaino26@gmail.com>
2026-08-14 21:01:29 -0500

    feat: add initial TryFrom implementation

--- src/message/airborne_position.rs
+++ src/message/airborne_position.rs
@@ -10,3 +10,14 @@ pub struct AirbornePosition {
     icao_address: IcaoAddress,
     altitude: Altitude,
 }
+
+impl TryFrom<&RawFrame> for AirbornePosition {
+    type Error = AdsbError;
+    fn try_from(frame: &RawFrame) -> Result<Self, Self::Error> {
+        let altitude = Altitude::try_from(frame)?;
+        Ok(Self {
+            icao_address: frame.icao(),
+            altitude,
+        })
+    }
+}
