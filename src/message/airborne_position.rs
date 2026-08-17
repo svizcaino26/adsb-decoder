549468a1ac461f8aea4e82c4235f8fc247b3c495
Stephen Vizcaino <svizcaino26@gmail.com>
2026-08-16 20:23:34 -0500

    feat: add function to reorder the encoded altitude bits into Gillham encoded bits

--- src/message/airborne_position.rs
+++ src/message/airborne_position.rs
@@ -41,6 +41,40 @@ impl Altitude {
         let left_bits = (value >> 5) & 0x7F;
         (left_bits << 4) | (right_bits)
     }
+
+    /// Reorders the 12 bit encoded altitude field into Gillham coded bits.
+    ///
+    /// Original order:
+    /// `C1 A1 C2 A2 C4 A4 B1 D1 B2 D2 B4 D4`
+    ///
+    /// Reordered:
+    /// `D1 D2 D4 A1 A2 A4 B1 B2 B4 C1 C2 C4`
+    /// The Q bit `D1` is discarded.
+    ///
+    /// Two sets of values are returned.
+    /// - `gc500` encodes the coarse altitude in 500 feet increments.
+    /// - `gc100` encodes the fine altitude adjustment in 100 feet increments.
+    ///
+    /// Sources:
+    /// - <https://aviation.stackexchange.com/questions/97210/adsb-df17-altitude-decoding>
+    /// - <https://grokipedia.com/page/Gillham_code>
+    const fn reorder_gillham_bits(bits: i32) -> (i32, i32) {
+        let reordered = bits >> 7 & 0x01 // C4 LSB
+            | bits >> 8 & 0x02  // C2
+            | bits >> 9 & 0x04  // C1
+            | bits << 2 & 0x08  // B4
+            | bits << 1 & 0x10  // B2
+            | bits & 0x20       // B1
+            | bits & 0x40       // A4
+            | bits >> 1 & 0x80  // A2
+            | bits >> 2 & 0x100 // A1
+            | bits << 9 & 0x200 // D4
+            | bits << 8 & 0x400; // D2 MSB
+
+        let (gc500, gc100) = ((reordered >> 3) & 0xFF, reordered & 0x07);
+
+        (gc500, gc100)
+    }
 }
 
 impl TryFrom<&RawFrame> for Altitude {
@@ -56,6 +90,7 @@ impl TryFrom<&RawFrame> for Altitude {
                     let n = Self::remove_qbit(encoded_altitude);
                     Ok(Self::Barometric(Self::decode_barometric_altitude(n)))
                 } else {
+                    let (gc500, gc100) = Self::reorder_gillham_bits(encoded_altitude);
                 }
             }
             20..=22 => Ok(Self::Geometric(777)),
