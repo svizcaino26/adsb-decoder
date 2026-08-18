f8c1ce19c9d349d956f6d3c91b3ce66cfc584a51
Stephen Vizcaino <svizcaino26@gmail.com>
2026-08-18 14:06:39 -0500

    feat: add intermediate calculation of altitude zone index j defined by spec

--- src/message/airborne_position.rs
+++ src/message/airborne_position.rs
@@ -185,12 +185,24 @@ pub struct AirbornePosition {
 }
 
 impl AirbornePosition {
-    const fn latitude_zone_index() {}
-    const fn decode_global_position(odd: &Odd, even: &Even) -> Self {
-        Self {
+    #[allow(
+        clippy::as_conversions,
+        clippy::cast_precision_loss,
+        clippy::cast_possible_truncation
+    )]
+    const fn latitude_zone_index(odd: &Odd, even: &Even) -> i32 {
+        let lat_even = even.lat_cpr as f64 / CPR_SCALE;
+        let lat_odd = odd.lat_cpr as f64 / CPR_SCALE;
+
+        f64::floor(59.0 * lat_even - 60.0 * lat_odd + 0.5) as i32
+    }
+
+    const fn decode_global_position(odd: &Odd, even: &Even) -> Result<Self, AdsbError> {
+        let j = Self::latitude_zone_index(odd, even);
+        Ok(Self {
             latitude: 1.0,
             longitude: 1.0,
-        }
+        })
     }
 }
 
