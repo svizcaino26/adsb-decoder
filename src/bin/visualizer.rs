0f75b2d01a38f000dea8c1f2ace8f15dec0d257a
Stephen Vizcaino <svizcaino26@gmail.com>
2026-08-26 21:23:59 -0500

    feat: add frame decoding flow from file source and logic for drawing and processing

--- src/bin/visualizer.rs
+++ src/bin/visualizer.rs
@@ -2,13 +2,22 @@ use std::fs::File;
 
 use adsb_decoder::{
     message::Message,
-    source::{FileSource, FrameSource, SourceError},
+    source::{FileSource, FileSourceError, FrameSource},
     tracker::AircraftTracker,
 };
 use macroquad::prelude::*;
 
+/// Interval for processing frames from source 1 every second.
+const FRAME_INTERVAL: f32 = 1.0;
+/// Interval for running `prune()` method on `AircraftTracker`.
+const PRUNE_INTERVAL: f32 = 30.0;
+
 #[macroquad::main("Visualizer")]
 async fn main() {
+    let mut frame_elapsed = 0.0;
+    let mut prune_elapsed = 0.0;
+    let mut delta_time;
+
     let mut source =
         FileSource::from_file(File::open("rawframes.txt").expect("Raw frames file not found"));
 
@@ -16,23 +25,25 @@ async fn main() {
 
     loop {
         clear_background(BLACK);
+        delta_time = get_frame_time();
+        frame_elapsed += delta_time;
+        prune_elapsed += delta_time;
 
-        let frame = source.next_frame();
-        match frame {
-            Ok(Some(frame)) => {
-                let message = Message::try_from(&frame);
+        if frame_elapsed >= FRAME_INTERVAL {
+            frame_elapsed -= FRAME_INTERVAL;
+            let frame = source.next_frame();
 
-                match message {
-                    Ok(msg) => match tracker.update(msg) {
-                        Ok(_) => (),
-                        Err(_) => (),
-                    },
-                    Err(_) => (),
-                }
+            match frame {
+                Ok(Some(frame)) => eprintln!("{frame:?}"), //println!("{frame:?}"),
+                Ok(None) => (),
+                Err(FileSourceError::Io(e)) => eprint!("{e}"),
+                Err(FileSourceError::Adsb(e)) => eprintln!("{e}"),
             }
-            Ok(None) => (),
-            Err(SourceError::Adsb(_)) => (),
-            Err(SourceError::IO(_)) => (),
+        }
+
+        if prune_elapsed >= PRUNE_INTERVAL {
+            prune_elapsed -= PRUNE_INTERVAL;
+            tracker.prune();
         }
         next_frame().await;
     }
